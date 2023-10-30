e0cde0fbebf6d68569cadd25cc01cbbf1550308a
yoons02 <kys5037k@naver.com>
2023-10-31 00:13:04 +0900

    feat: User model point field 추가

--- BackEnd/EcoBack/main/serializers.py
+++ BackEnd/EcoBack/main/serializers.py
@@ -6,6 +6,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+    
+    image = serializers.ImageField(use_url=True)
 
 class EventSerializer(serializers.ModelSerializer):
 
