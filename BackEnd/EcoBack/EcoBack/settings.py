f78bcc1ec9261b375788c1c03e05eadb5ed04485
yoons02 <kys5037k@naver.com>
2023-10-31 10:04:01 +0900

    fix: change file structure

--- BackEnd/EcoBack/EcoBack/settings.py
+++ BackEnd/EcoBack/EcoBack/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',  # 이 부분을 추가
 ]
 
 ROOT_URLCONF = 'EcoBack.urls'
