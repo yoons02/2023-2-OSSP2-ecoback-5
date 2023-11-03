738299ad326d024535804bbf71514cb55abe01f4
kitae9999 <gitae9999@naver.com>
2023-11-03 23:18:47 +0900

    feat:로그인페이지 디자인 완성

--- FrontEnd/React/ecoback/src/pages/Login.js
+++ FrontEnd/React/ecoback/src/pages/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import logo from '../image/logo.png'; // 로고 이미지 경로
 import loginbutton from '../image/loginbutton.png';
+import googleloginbutton from '../image/googleloginbutton.png';
+
 import '../css/Login.css';
 
 function Login() {
@@ -13,33 +15,45 @@ function Login() {
   };
 
   return (
-    <center>
-        <div id="logo">
-        <img src={logo} alt="logo" />
-        <br/>
-        플라스틱으로 힘들어 하고 있는 바다거북이를 지켜주세요!
-        <br/>
-        <br/>
-        <div>
-            <label id="id_input">
-            아이디<br/>
-            <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-            </label>
-        </div>
-        <div>
-            <label id="password_input">
-            비밀번호
-            <br/>
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-        </div>
-        {/* <button onClick={handleLogin}>로그인하기</button> */}
-        <a href="./Home">
-            <img src={loginbutton} alt="loginbutton"/>
-        </a>
-        <hr/>
-        </div>
-    </center>
+    <html>
+        <br></br><br></br><br></br><br></br><br></br>
+        <center>
+            <div id="logo">
+                <img src={logo} alt="logo" />
+                <br/>
+                플라스틱으로 힘들어 하고 있는 바다거북이를 지켜주세요!
+                <br/>
+                <br/>
+            </div>
+        </center>
+        <p class="ip">아이디</p>
+        <center>
+            <div id="id_input">
+                <input type="text" value={id} onChange={(e) => setId(e.target.value)}  placeholder="아이디를 입력하세요" />
+            </div>
+            <br></br>
+        </center>
+        <p class="ip">비밀번호</p>
+        <center>
+            <div id="password_input">  
+                <input type="password"  value={password} onChange={(e) => setPassword(e.target.value) }  placeholder="비밀번호를 입력하세요" />           
+            </div>
+        </center>
+        <br></br>
+        <br></br>
+
+        <center>
+            <a href="./Home">
+                <img src={loginbutton} alt="loginbutton"/>
+            </a>
+        </center>
+        <p class="ask_register">계정이 없으신가요? 가입하기 &nbsp;&nbsp;</p> 
+        {/* 가입하기 누르면 가입창 나오게 제작예정 */}
+        <center>
+            <div class="hr-sect">또는</div>
+            <img src={googleloginbutton} alt="googleloginbutton"/>
+        </center>
+    </html>
   );
 }
 
