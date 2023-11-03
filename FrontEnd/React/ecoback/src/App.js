3dec34f9073eb7bd440507395a5e95cf397ae47b
kitae9999 <gitae9999@naver.com>
2023-11-03 20:21:44 +0900

    feature: 로그인페이지 기초 구성

--- FrontEnd/React/ecoback/src/App.js
+++ FrontEnd/React/ecoback/src/App.js
@@ -1,16 +1,20 @@
 import {Route, Routes} from 'react-router-dom';
 import Home from '../src/pages/Home.js';
 import Store from '../src/pages/Store.js';
+import Login from './pages/Login.js';
 
 
 
 const App = () => {
   return(
+    
     <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login/>}/>
+        <Route path="/Home" element={<Home />} />
         <Route path="/Store" element={<Store />} />
     </Routes> 
   );
 };
+
 
 export default App;
