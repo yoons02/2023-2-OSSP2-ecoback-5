00ecf49a39b13695db73e1b59884453d4ec2f1f1
yoons02 <kys5037k@naver.com>
2023-11-03 16:17:03 +0900

    fix: accounts api

--- BackEnd/EcoBack/accounts/urls.py
+++ BackEnd/EcoBack/accounts/urls.py
@@ -6,5 +6,5 @@ app_name = "accounts"
 urlpatterns = [
     path('accounts/', include('dj_rest_auth.urls')),
     path('accounts/', include('dj_rest_auth.registration.urls')),
-    path('accounts/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('accounts/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
