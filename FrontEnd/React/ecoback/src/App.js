0aba74a0172329545353617aa5645065b00a31e9
seulgibaek <seulgi.baek@dgu.ac.kr>
2023-11-03 21:15:28 +0900

    makingMyPage

--- FrontEnd/React/ecoback/src/App.js
+++ FrontEnd/React/ecoback/src/App.js
@@ -1,11 +1,11 @@
 import {Route, Routes} from 'react-router-dom';
 import Home from '../src/pages/Home.js';
 import Store from '../src/pages/Store.js';
-<<<<<<< HEAD
+
 import MyPage from './seulgi/mypage.js';
-=======
+
 import Login from './pages/Login.js';
->>>>>>> upstream/main
+
 
 
 
@@ -13,13 +13,13 @@ const App = () => {
   return(
     
     <Routes>
-<<<<<<< HEAD
+
       <Route path="/" element={<MyPage />} />
         {/* <Route path="/" element={<Home />} /> */}
-=======
+
         <Route path="/" element={<Login/>}/>
         <Route path="/Home" element={<Home />} />
->>>>>>> upstream/main
+
         <Route path="/Store" element={<Store />} />
     </Routes> 
   );
