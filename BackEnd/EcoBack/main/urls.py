cfd8c6caeffb0a9a1abb6b0e1ce2ed439d58da51
yoons02 <kys5037k@naver.com>
2023-11-07 03:55:27 +0900

    feat: product read (urls.py)

--- BackEnd/EcoBack/main/urls.py
+++ BackEnd/EcoBack/main/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('events/', views.event_all),
     path('events/<int:id>/', views.event_detail),
     path('barcodes/', views.barcode_create),
-    path('maket/categories/', views.category_read),
+    path('market/categories/', views.category_read),
+    path('market/categories/<int:id>', views.product_read),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
