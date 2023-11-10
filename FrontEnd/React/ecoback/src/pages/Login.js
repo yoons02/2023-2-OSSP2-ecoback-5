96f34f925236eae2cd9eb8828e4a2e82c9daa037
kitae9999 <gitae9999@naver.com>
2023-11-10 19:38:15 +0900

    login.css 수정

--- FrontEnd/React/ecoback/src/pages/Login.js
+++ FrontEnd/React/ecoback/src/pages/Login.js
@@ -61,7 +61,7 @@ function Login() {
         </center>
         <br></br>
         <br></br>
-        <Link to="/register" class="ask_register">계정이 없으신가요? 가입하기 &nbsp;&nbsp;</Link>
+        <Link to="/register" ><p class="ask_register">계정이 없으신가요? 가입하기 &nbsp;&nbsp;</p></Link>
         {/* 가입하기 누르면 가입창 나오게 제작예정 */}
         <center>
             <a href="./Home">
