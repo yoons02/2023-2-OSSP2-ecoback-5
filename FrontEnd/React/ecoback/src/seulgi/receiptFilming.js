7f8f2c215e0fba464be964662d88377162afaf34
kitae9999 <gitae9999@naver.com>
2023-11-13 23:59:30 +0900

    상단바 ui 크기 통일

--- FrontEnd/React/ecoback/src/seulgi/receiptFilming.js
+++ FrontEnd/React/ecoback/src/seulgi/receiptFilming.js
@@ -9,10 +9,14 @@ const ReceiptFilming=()=>{
     <div class="filmingPage" style={{backgroundColor:"#F9F9F9"}}>
         <TitleBanner />
             <hr />
-            <div class="top_line">
+            {/* <div class="top_line">
                 <Backbutton />
                 <div class="normal" style={{fontSize: '30px',margin:'auto 0'}}>영수증 촬영</div>
-            </div>
+            </div> */}
+            <div style={{ display: 'flex' }}>
+                <Backbutton/>
+                <p id="film_title">영수증 촬영</p>    
+            </div> 
             <div id="camera">
                 <Camera />
             </div>
