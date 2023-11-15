660b77e771a85195fcd3e0d31a0b166ae587a83e
kitae9999 <gitae9999@naver.com>
2023-11-15 15:31:11 +0900

    백버튼바 관련 코드 정리

--- FrontEnd/React/ecoback/src/seulgi/BeautyHealthCategory.js
+++ FrontEnd/React/ecoback/src/seulgi/BeautyHealthCategory.js
@@ -8,14 +8,6 @@ const BeautyHealthCategory=()=>{
         <div>
         <TitleBanner />
         <hr/>
-        {/* <div class="top_line">
-        <img
-          src={require('../image/arrow-right.png')}
-          alt="receipt"
-          style={{ width: '30px', height: '30px' }}
-        />
-        <div class="normal" style={{fontSize: '5vw',margin:'auto 0'}}>뷰티/패션/건강</div>
-      </div> */}
       <BackbuttonBar title="뷰티/패션/건강"/>
       <hr></hr>
       <div>
--- FrontEnd/React/ecoback/src/seulgi/GiftcardCategory.js
+++ FrontEnd/React/ecoback/src/seulgi/GiftcardCategory.js
@@ -8,14 +8,7 @@ const GiftcardCategory=()=>{
         <div>
         <TitleBanner />
         <hr/>
-        {/* <div class="top_line">
-        <img
-          src={require('../image/arrow-right.png')}
-          alt="receipt"
-          style={{ width: '30px', height: '30px' }}
-        />
-        <div class="normal" style={{fontSize: '5vw',margin:'auto 0'}}>아이스크림/빙수</div>
-      </div> */}
+       
       <BackbuttonBar title="기프트카드/상품권"/>
       <hr></hr>
       <div>
