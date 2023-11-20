a10854d17bbb90fbe4cd685c9c840365370059ce
namminju <nampink2300@gmail.com>
2023-11-20 17:48:58 +0900

    fix: store_main dummy data 이용하여  axios 형태로  link

--- FrontEnd/React/ecoback/src/components/store_box.js
+++ FrontEnd/React/ecoback/src/components/store_box.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Store.css';
-import category from '../minju/productCategories.json';
 import axios from 'axios';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Store_box = () => {
+  const [category, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('/productCategories.json');
+      setEvents(response.data); // Use response.data instead of data
+    } catch (error) {
+      console.error('API 오류', error);
+    }
+  };
+
   return (
     <div className='title_line'>
       {category.map((categoryData, index) => (
         <div key={index}>
-
-          <Link to={"/"+categoryData.name}>
-          <div className="box_categories_m">
-            <img style={{width: '18vw'}} src={require('../image/'+categoryData.image)} alt={categoryData.name} />
-          </div>
+          <Link to={'/' + categoryData.name}>
+            <div className='box_categories_m'>
+              <img style={{ width: '18vw' }} src={require('../image/' + categoryData.image)} alt={categoryData.name} />
+            </div>
           </Link>
-          <div className='normal' style={{textAlign:'center'}}>{categoryData.name}</div>
-        
+          <div className='normal' style={{ textAlign: 'center' }}>{categoryData.name}</div>
         </div>
       ))}
     </div>
