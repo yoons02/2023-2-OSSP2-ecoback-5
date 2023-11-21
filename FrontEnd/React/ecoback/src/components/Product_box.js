abae5a9a12f9fec78e1321f17f0af77b1220d641
namminju <nampink2300@gmail.com>
2023-11-21 16:21:54 +0900

    feat: 개별 상품 페이지 생성하는 component 생성

---
+++ FrontEnd/React/ecoback/src/components/Product_box.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import '../css/Store.css';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import BackbuttonBar from 'components/BackbuttonBar';
+import TitleBanner from "./TitleBanner.js";
+const Store_box = ({url, title}) => {
+  const [category, setEvents] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setEvents(response.data); 
+    } catch (error) {
+      console.error('API 오류', error);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <TitleBanner />
+      </div>
+      <hr/>
+      <BackbuttonBar title={title}/>
+      <hr></hr>
+    <div className='title_line_P'>
+      {category.map((categoryData, index) => (
+        <div key={index}>
+          <Link to={'./' + categoryData.id}>
+            <div className='box_categories_P'>
+              <img style={{ width: '21vw' }} src={require('../image/categories/' + categoryData.product_image)} alt={categoryData.name} />
+            </div>
+
+          <div className='normal' style={{ textAlign: 'center', fontWeight:'normal'}}>{categoryData.name}</div>
+          <div className='normal' style={{ textAlign: 'center' }}>{categoryData.price}원</div>
+          </Link>
+        </div>
+      ))}
+    </div>
+     
+    </div>
+  );
+};
+
+export default Store_box;
