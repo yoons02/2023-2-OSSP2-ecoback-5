4115afdd49c6692bbf70c39bf93572e833ace58b
seulgiBaek <seulgi.baek@dgu.ac.kr>
2023-12-07 10:35:25 +0900

    바코드 사진 업로드

--- FrontEnd/React/ecoback/src/seulgi/Camera.js
+++ FrontEnd/React/ecoback/src/seulgi/Camera.js
@@ -1,178 +1,3 @@
-// /*웹브라우저 보안 정책상 웹애플리케이션이 사용자 파일 시스템에 직접적으로 접근하여
-// 파일을 저장할 수 없다고 함. 대신 사용자에게 캡처된 이미지를 다운로드할 수 있도록
-// 유도할 수 있는 기능을 찾아 코드를 추가해놓음.
-
-// ->Refresh버튼:원래 없는건데 사진 캡처하고 사용자가 수동으로 이미지 저장할 수 있게 화면에 띄워놓고
-// Refrsh 버튼 누르면 사진 사라지게 일단 임시로 버튼 만들어놓음.
-// */
-
-// import React, { useState, useEffect, useRef, useCallback } from 'react';
-// import Webcam from 'react-webcam';
-// import './receiptFilming.css';
-// import SendPhotoModal from './sendPhotoModal';
-// import API from 'api/axios';
-
-// const videoConstraints = {
-//   width: { ideal: window.innerWidth },
-//   height:{ideal:window.innerHeight},
-//   facingMode: "user",
-//   /*전면카메라(PC용): user, 후카메라(모바일용): environment*/
-// };
-
-// const Camera = () => {
-//   const [isModalOpen, setIsModalOpen]=useState(false);
-//   const handleOpenModal=()=>setIsModalOpen(true);
-//   const webcamRef = useRef(null);
-//   const [url, setUrl] = useState(null);
-
-//   const capturePhoto = useCallback(() => {
-//     const imageSrc = webcamRef.current.getScreenshot();/*스크린샷 url 저장*/
-//     setUrl(imageSrc);
-
-//     // 캡처된 이미지를 다운로드하기 위한 링크 요소를 생성
-//     const downloadLink = document.createElement('a');
-//     downloadLink.href = imageSrc;
-//     downloadLink.download = 'barcode.png'; // 다운로드될 파일의 이름을 지정
-
-//     // 링크를 문서에 추가하고 클릭 이벤트를 발생
-//     document.body.appendChild(downloadLink);
-//     downloadLink.click();
-
-//     // 링크 요소를 다시 문서에서 제거
-//     document.body.removeChild(downloadLink);
-//   }, [webcamRef]);
-
-//   const onUserMedia = (e) => {
-//     console.log("User Media accessed", e);
-//   }
-//   const dataURLtoFile = (dataurl, filename) => {
-//     let arr = dataurl.split(',');
-//     let mime = arr[0].match(/:(.*?);/)[1];
-//     let bstr = atob(arr[1]);
-//     let n = bstr.length;
-//     let u8arr = new Uint8Array(n);
-
-//     while (n--) {
-//         u8arr[n] = bstr.charCodeAt(n);
-//     }
-
-//     // Blob 대신 File 객체를 생성합니다.
-//     return new File([u8arr], filename, { type: mime });
-// };
-
-// const sendPhotoServer = async () => {
-
-//     const imageFile = dataURLtoFile(url, 'barcode.png');
-//     const formData = new FormData();
-
-//     formData.append('status', 'approved');
-//     formData.append('code', '0036000291452');
-//     formData.append('image', imageFile);
-
-//     const endpoint="/barcodes/";
-//     const access_token=localStorage.getItem('access');
-//     console.log("accessToken: ",access_token);
-    
-//     try{
-//       /*post 형식으로 formData 전송*/
-//       const response=await API.post(endpoint,formData,{
-//         headers:{
-//           'Authorization':`Bearer ${access_token}`,
-//         }
-//       });
-
-//       if(response.status === 200){
-//         console.log('Photo uploaded Successfully!');
-//       }
-//       else{
-//         console.log('Fail to Upload Photo.');
-//       }
-//     }
-//     catch(e){
-//       console.error('Error: ',e);
-//     }
-// };
-
-
-//   // //데이터 URL에서 Blob 객체 생성하는 함수
-//   // //Base64 인코딩된 데이터 url를 'Blob' 객체로 변환해 파일 형식으로 서버에 전달.
-//   // const dataURLtoBlob=(dataurl)=>{
-//   //   let arr=dataurl.split(','),mime=arr[0].match(/:(.*?);/)[1],
-//   //   bstr=atob(arr[1]),n=bstr.length, u8arr=new Uint8Array(n);
-//   //   while(n--){
-//   //     u8arr[n]=bstr.charCodeAt(n);
-//   //   }
-//   //   return new Blob([u8arr],{type:mime});
-//   // }
-//   // /*서버로 사진 전송하는 함수*/
-//   // const sendPhotoServer = async()=>{
-//   //   handleOpenModal();
-//   //   const imageBlob=dataURLtoBlob(url);
-//   //   const formData=new FormData();
-//   //   /*<input name="photo" value={url} />이라고 생각하면 됨. */
-//   //   formData.append('status','approved');
-//   //   formData.append('code','0036000291452');
-//   //   formData.append('image',imageBlob, 'barcode.png');
-
-//   //   const endpoint="/barcodes";
-//   //   const access_token=localStorage.getItem('access');
-//   //   console.log("accessToken: ",access_token);
-    
-//   //   try{
-//   //     /*post 형식으로 formData 전송*/
-//   //     const response=await API.post(endpoint,formData,{
-//   //       headers:{
-//   //         'Authorization':`Bearer ${access_token}`,
-//   //       }
-//   //     });
-
-//   //     if(response.status === 200){
-//   //       console.log('Photo uploaded Successfully!');
-//   //     }
-//   //     else{
-//   //       console.log('Fail to Upload Photo.');
-//   //     }
-//   //   }
-//   //   catch(e){
-//   //     console.error('Error: ',e);
-//   //   }
-
-//   // }
-
-//   return (
-//     <>
-//       <Webcam
-//         class="webcam"
-//         ref={webcamRef}
-//        audio={false}
-//         screenshotFormat="image/png"
-//         videoConstraints={videoConstraints}
-//        onUserMedia={onUserMedia}
-//         mirrored={true}
-//         width="100%"
-//       />
-
-//       <div className="container">
-//       <button id="album"><img id="album" src={require("../image/photoAlbum.png")} alt="photoAlbum" /></button>
-//       <button id="turtleBtn" onClick={capturePhoto}><img id="turtleBtn" src={require("../image/turtleBtn.png")} alt="captureBtn" /></button>
-//       <button id="refresh" onClick={() => { setUrl(null) }}><img id="refreshBtn"src={require("../image/refreshBtn.png")}/></button>
-//       </div>
-    
-
-//       {url && (
-//         <div id="screenShot">
-//           <img src={url} alt='ScreenShot'/>
-//           <button id="sendPhotoBtn" onClick={sendPhotoServer}>전송하기</button>
-//         </div>
-//       )}
-     
-//     <SendPhotoModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
-//     </>
-//   );
-// }
-
-// export default Camera;
-
 /*웹브라우저 보안 정책상 웹애플리케이션이 사용자 파일 시스템에 직접적으로 접근하여
 파일을 저장할 수 없다고 함. 대신 사용자에게 캡처된 이미지를 다운로드할 수 있도록
 유도할 수 있는 기능을 찾아 코드를 추가해놓음.
@@ -185,6 +10,7 @@ import Webcam from 'react-webcam';
 import './receiptFilming.css';
 import SendPhotoModal from './sendPhotoModal';
 import API from 'api/axios';
+import { IoEllipseSharp } from 'react-icons/io5';
 
 const videoConstraints = {
   width: { ideal: window.innerWidth },
@@ -199,6 +25,7 @@ const Camera = () => {
   const webcamRef = useRef(null);
   const [url, setUrl] = useState(null);
   const [responseStatus, setResponseStatus]=useState(null);
+  const [selectedFile, setSelectedFile]=useState(null);// 바코드 사진 업로드용
 
   const capturePhoto = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();/*스크린샷 url 저장*/
@@ -259,7 +86,7 @@ const Camera = () => {
       setResponseStatus(response.data.status);
   
       if (response.data.status === "approved") {
-        alert("바코드 전송에 성공하였습니다.");
+        /*모달창으로 대체*/
         /*duplicate: 중복된 요청, 이미 서버에 저장된 이미지인 경우*/
       } else if (response.data.status === "duplicate") {
         alert("duplicate: 이미 등록된 바코드입니다.");
@@ -273,6 +100,66 @@ const Camera = () => {
       alert("오류 발생");
     }
   };
+  /*갤러리 접근이 잘 안돼서 우선 file 업로드 방향으로 수정하였습니다.*/
+  const handleFileSelect=(e)=>{
+    const file=e.target.files[0];
+    if(file){
+      setSelectedFile(file);/*selectedFile*/
+      console.log(file);
+    }
+  }
+
+  
+  const handleAlbumClick =()=>{
+    const fileInput=document.createElement('input');
+    fileInput.type = 'file';
+    fileInput.accept='image/*';//image 파일만 선택 가능하도록 설정
+    fileInput.onchange=handleFileSelect;
+    /*사진 선택 후 uploadFile 진행*/
+    uploadFile();
+  }
+
+  const uploadFile = async () => {
+    
+    if(selectedFile){
+      const formData=new FormData();
+      formData.append('file',selectedFile);
+
+    const endpoint = "/barcodes/";
+    const access_token = localStorage.getItem('access');
+  
+
+        try {
+          /* post 형식으로 formData 전송 */
+          const response = await API.post(endpoint, formData, {
+            headers: {
+              'Authorization': `Bearer ${access_token}`,
+              'Content-Type': 'multipart/form-data' // formData 형식으로 파일 전달
+            }
+          });
+      
+          console.log("response.data: ", response.data);
+          setResponseStatus(response.data.status);
+      
+          if (response.data.status === "approved") {
+            alert("바코드 전송에 성공하였습니다.");
+            /*duplicate: 중복된 요청, 이미 서버에 저장된 이미지인 경우*/
+          } else if (response.data.status === "duplicate") {
+            alert("duplicate: 이미 등록된 바코드입니다.");
+          } else if (response.data.status === "invalid") {
+            alert("텀블러 미사용 바코드로 적립이 불가합니다.");
+          } else{
+            alert("바코드 정보를 인식할 수 없습니다.");
+          }
+        } catch (error) {
+          console.error('Error: ', error);
+          alert("오류 발생");
+        }
+    } else{
+      alert("file이 선택되지 않았습니다.");
+    }
+  
+    
   
   return (
     <>{url?(<div id="screenShot">
@@ -292,10 +179,9 @@ const Camera = () => {
         width="100%"
       />
     )}
-      
-  
+    
       <div className="container">
-        <button id="album"><img id="album" src={require("../image/photoAlbum.png")} alt="photoAlbum" /></button>
+        <button id="album"><img id="album" onClick={handleAlbumClick} src={require("../image/photoAlbum.png")} alt="photoAlbum" /></button>
         <button id="turtleBtn" onClick={capturePhoto}><img id="turtleBtn" src={require("../image/turtleBtn.png")} alt="captureBtn" /></button>
         <button id="refresh" onClick={() => { setUrl(null) }}><img id="refreshBtn"src={require("../image/refreshBtn.png")}/></button>
       </div>
