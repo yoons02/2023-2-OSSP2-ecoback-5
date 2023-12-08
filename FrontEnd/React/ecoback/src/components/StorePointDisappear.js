8d9a055a3b7d1daabac1e2eeed3414b9830ba1e3
namminju <nampink2300@gmail.com>
2023-12-08 13:51:13 +0900

    fix: 세부 브랜드 &  세부 상품 & 개별 상품 페이지 생성 수정

--- FrontEnd/React/ecoback/src/components/StorePointDisappear.js
+++ FrontEnd/React/ecoback/src/components/StorePointDisappear.js
@@ -27,17 +27,20 @@ const StorePointDisappear = () => {
 
   // 날짜 형식 변환을 위한 함수
   const formatDate = (dateString) => {
+    if (!dateString || isNaN(Date.parse(dateString))) {
+      return "소멸 예정인 포인트가 없습니다.";
+    }
     const date = new Date(dateString);
     const year = date.getFullYear();
     const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 1을 더하고 두 자리로 패딩
     const day = date.getDate().toString().padStart(2, '0');
-    return `${year}년 ${month}월 ${day}일`;
+    return `포인트가 ${year}년 ${month}월 ${day}일에 소멸될 예정이에요.`;
   };
 
   return (
     <div className="full_container">
       <div className="normal" style={{ textAlign: 'center', fontSize: '90%', margin: '10%' }}>
-        포인트가 {formatDate(categories['extinction period'])}에 소멸될 예정이에요.
+        {formatDate(categories['extinction period'])}
       </div>
     </div>
   );
---
+++ FrontEnd/React/ecoback/src/pages/SpecificProductDetail.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from 'react';
+import '../css/Product.css';
+import css from '../css/Product.css';
+import TitleBanner from '../components/TitleBanner';
+import DolceLatte from '../image/dolcelatte.png';
+import Check from '../image/check.png';
+import BuyingPopup from './BuyingPopup';
+import BackbuttonBar from 'components/BackbuttonBar';
+import API from 'api/axios';
+import { useLocation } from 'react-router-dom';
+const Product = () => {
+  const [product, setProduct] = useState([]);
+  const location = useLocation();
+  const id = location.pathname.split('/').pop(); 
+
+  useEffect(() => {
+    fetchProductData();
+  }, []);
+
+  const fetchProductData = async () => {
+    const access_token = localStorage.getItem('access');
+    try {
+      const response = await API.get(`/market/products/${id}/`, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      });
+      setProduct(response.data);
+    } catch (e) {
+      console.log('API 오류: ', e);
+    }
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = () => setIsModalOpen(true);
+
+  return (
+    <div className="full_container">
+      <div  style={{ minHeight: '84vh' }}>
+      <div>
+        <TitleBanner />
+      </div>
+      <hr />
+      <BackbuttonBar title="개별 상품 페이지" />
+      <hr />
+      <div className="productBox">
+        <div className="Item">
+          <img id="itemImg" src={product.product_image} alt={product.name} />
+        </div>
+        <div style={{ fontSize: '25px' }}>
+          <b>{product.name}</b>
+        </div>
+        <div className="priceTag">
+          <div id="oneline">
+            <div id="resell">리셀 상품</div>
+            <img src={Check} alt="check" id="check" />
+          </div>
+          <div id="class" style={{ fontSize: '30px' }}>
+            <b>{product.price} 원</b>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div className="btnBox">
+        <button id="buyBtn" onClick={() => handleOpenModal()}>
+          <b style={{ fontSize: '15px' }}>구매하기</b>
+        </button>
+        <BuyingPopup isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} price={product.price} />
+      </div>
+    </div>
+  );
+};
+
+export default Product;
