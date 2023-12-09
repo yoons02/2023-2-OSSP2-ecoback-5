ffb45334d821ec37f6a1b4bddc9457f3a55176a6
namminju <nampink2300@gmail.com>
2023-12-09 14:31:45 +0900

    fix: 상품 구매시 구매한 상품에 추가되게 변경 및 팝업창 css 수정

--- FrontEnd/React/ecoback/src/pages/paymentSuccess.js
+++ FrontEnd/React/ecoback/src/pages/paymentSuccess.js
@@ -5,14 +5,17 @@ import '../seulgi/sendPhotoModal.css';
 const PaymentSuccessModal=({isModalOpen, setIsModalOpen})=> {
     const modalStyle = {
         content: {
-            alignItems:"center",
-            borderRadius: "2vw",
-            maxHeight:"60vw",
-            position:'absolute',
-            top:"25%",
-            left:"13%",
-
-        },
+          borderRadius: "20px",
+          aspectRatio: '1 / 1',
+          maxHeight: "40vh",
+          position: 'fixed',
+          width: '100%',
+          maxWidth: '300px',
+          margin: 0,
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+      },
         overlay:{
             backgroundColor:'rgba(0, 0, 0, 0.4)'
         }
@@ -27,7 +30,7 @@ const PaymentSuccessModal=({isModalOpen, setIsModalOpen})=> {
           <button id="xbutton" onClick={()=>{handleCloseModal()}}>X</button>
           </div>
           <div className="middle">
-            <img style={{width: "40vw"}}src="https://img.freepik.com/free-vector/seasonal-sale-discounts-presents-purchase-visiting-boutiques-luxury-shopping-price-reduction-promotional-coupons-special-holiday-offers-vector-isolated-concept-metaphor-illustration_335657-2766.jpg?w=740&t=st=1701356185~exp=1701356785~hmac=8a924dfccef73c6f358e328c09853e251da311964d75089dc3375510ad0c8671" alt="barcode"/>
+            <img style={{width: "50%"}}src="https://img.freepik.com/free-vector/seasonal-sale-discounts-presents-purchase-visiting-boutiques-luxury-shopping-price-reduction-promotional-coupons-special-holiday-offers-vector-isolated-concept-metaphor-illustration_335657-2766.jpg?w=740&t=st=1701356185~exp=1701356785~hmac=8a924dfccef73c6f358e328c09853e251da311964d75089dc3375510ad0c8671" alt="barcode"/>
             <div className="text"><b>상품 구매가 완료되었습니다.</b></div>
           </div>
           </Modal>
